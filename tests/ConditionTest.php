d9cb1264729810fe232d9318800f4f98a08182db
Dionicio Acevedo <tec.dionicioacevedo@gmail.com>
2025-01-12 21:20:13 -0400

    adding additional test for conditions

--- tests/ConditionTest.php
+++ tests/ConditionTest.php
@@ -22,4 +22,36 @@ final class ConditionTest extends TestCase
 
         (new Equals())->evaluate();
     }
+
+    public function test_condition_returns_expected_result() 
+    {
+        $this->assertTrue((new Equals())->args([1, 1])->evaluate());
+        $this->assertFalse((new Equals())->args([1, 2])->evaluate());
+    }
+
+    public function test_condition_can_be_negated()
+    {
+        $negation = (new Equals())->args([1, 2])->negate();
+
+        $this->assertInstanceOf(\Condiment\Evaluables\Operators\Negation::class, $negation);
+    }
+
+    public function test_negation_returns_the_opposite_result()
+    {
+        $condition = new Equals();
+        $condition->args(["condiment", "condiment"]);
+
+        $this->assertEquals(true, $condition->evaluate());
+        $this->assertEquals(false, $condition->negate()->evaluate());
+
+        $condition = new Equals();
+        $condition->args(["Hello World", "condiment"]);
+
+        $this->assertEquals(false, $condition->evaluate());
+        $this->assertEquals(true, $condition->negate()->evaluate());
+
+        
+    }
+
 }
+
