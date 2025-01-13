1cde29ee75b1c684be9358804e387c78a45b46e7
Dionicio Acevedo <tec.dionicioacevedo@gmail.com>
2025-01-12 20:45:31 -0400

    use file to test conditions

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ use Condiment\Evaluables\Evaluator;
 
 require __DIR__ . '/vendor/autoload.php';
 
+error_reporting(E_ALL);
+
 $conditionsArray = json_decode(file_get_contents('test_condition.json'), TRUE);
 
 
@@ -10,14 +12,17 @@ function sanitize_regex(string $regex): string
 {
     return preg_replace_callback("/[#-.]|[[-^]|[?|{}]/", fn($match) => "\\$match[0]", $regex);
 }
-
-$evaluator = new Evaluator();
-
-$evaluator->addEvaluablesFromArray($conditionsArray);
-
-
-if ($evaluator->evaluate()) {
-    echo "<h1>You are worth</h1>";
-}else {
-    echo "<h1>You deserve nothing, muggle</h1>";
+try {
+    $evaluator = new Evaluator();
+    $evaluator->addEvaluablesFromArray($conditionsArray);
+    
+    if ($evaluator->evaluate()) {
+        echo "<h1>You are worth</h1>";
+    }else {
+        echo "<h1>You deserve nothing, muggle</h1>";
+    }
+} catch (\Throwable $th) {
+    echo '<p style="color: red;">'.$th->getMessage().'</p>';
 }
+
+
