3119ee606cdcf7b2321655069c3d031480f06954
Dionicio Acevedo <tec.dionicioacevedo@gmail.com>
2025-02-26 21:32:16 -0400

    add support to compare numbers, dates, strings

--- src/Evaluables/Conditions/Definitions/Equals.php
+++ src/Evaluables/Conditions/Definitions/Equals.php
@@ -2,9 +2,9 @@
 
 namespace Condiment\Evaluables\Conditions\Definitions;
 
-use Condiment\Evaluables\Conditions\ConditionDefinition;
+use Condiment\Evaluables\Conditions;
 
-class Equals extends ConditionDefinition
+class Equals extends Conditions\ConditionDefinition
 {
     protected $stringCompareFunction = 'strcasecmp';
 
@@ -12,17 +12,22 @@ class Equals extends ConditionDefinition
     {
         list($a, $b) = $this->arguments;
 
-        if (is_numeric($a) && is_numeric($b))
-        {
+        if (is_numeric($a) && is_numeric($b)) {
             return $this->numberEquals((float)$a, (float)$b);
+        }else if ($a instanceof \DateTimeInterface && $b instanceof \DateTimeInterface) {
+            return $this->dateTimeEquals($a, $b);
         }
 
-        return call_user_func_array($this->stringCompareFunction, [$a, $b]) === 0;
+        return call_user_func_array($this->stringCompareFunction, [(string)$a, (string)$b]) === 0;
     }
 
 
-    public function numberEquals(float $a, float $b, $epsilon = 0.00001)
+    public function numberEquals(float $a, float $b, $epsilon = 0.00001): bool
     {
         return abs($a - $b) < $epsilon;
+    }
+
+    public function dateTimeEquals(\DateTimeInterface $a, \DateTimeInterface $b): bool {
+        return $a->format('Y-m-d H:i:s.u') === $b->format('Y-m-d H:i:s.u');
     }
 }
