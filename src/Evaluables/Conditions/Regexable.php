06b151563420047fb62560b503c2034361ed43be
Dionicio Acevedo <tec.dionicioacevedo@gmail.com>
2025-03-03 21:04:10 -0400

    chore: add docs to provide clarity

--- src/Evaluables/Conditions/Regexable.php
+++ src/Evaluables/Conditions/Regexable.php
@@ -6,6 +6,10 @@ namespace Condiment\Evaluables\Conditions;
 trait Regexable
 {
     /**
+     * Sanitize a value to avoid character to be interpreted on a regex
+     *
+     * For example a input "condi[]ment" it will be escaped to "condi\[\]ment"
+     *
      * @param string $input
      *
      * @return string
@@ -15,11 +19,27 @@ trait Regexable
         return preg_replace_callback("/[#-.]|[[-^]|[?|{}]/", fn($match) => "\\$match[0]", $input);
     }
 
+    /**
+     * Creates a regex with flags
+     *
+     * @param string $regex
+     * @param string $flags
+     *
+     * @return string
+     */
     public function regex(string $regex, $flags = ""): string
     {
         return sprintf("/%s/%s", $regex, $flags);
     }
 
+    /**
+     * @param string $subject
+     * @param string $regex
+     * @param  string
+     * @param bool $matchAll
+     *
+     * @return int
+     */
     public function matchInput(string $subject, string $regex, string $regexFlags ="", $matchAll = false): int
     {
         $matchFunction = $matchAll ? 'preg_match_all' : 'preg_match';
@@ -27,7 +47,7 @@ trait Regexable
         $matchCount = call_user_func_array(
             $matchFunction,
             [
-                $this->regex($regex),
+                $this->regex($regex, $regexFlags),
                 $subject,
             ]
         );
