c9e7603976b884aaad91a9e9c35ac4a86961fd5a
Dionicio Acevedo <tec.dionicioacevedo@gmail.com>
2025-03-18 21:40:43 -0400

    refactor: evaluator tests
    - Setup the evaluator instance to avoid initialization on each test
    - More robust test were added to evaluator to ensure consistency of the evaluator expected results
    - Added providers to test multiple scenarios

--- tests/EvaluatorTest.php
+++ tests/EvaluatorTest.php
@@ -1,33 +1,97 @@
 <?php
 
-use Condiment\Evaluables\Evaluator;
+use Condiment\Evaluables\Evaluators\Evaluator;
+use Condiment\Exceptions\EvaluatorInvalidConditionException;
 use PHPUnit\Framework\TestCase;
 
 final class EvaluatorTest extends TestCase
 {
- 
-    public function test_evaluator_can_be_instantiated_it()
+    private Evaluator $evaluator;
+
+    protected function setUp(): void
     {
-        $evaluator = new Evaluator();
-
-        $this->assertInstanceOf(Evaluator::class, $evaluator);
-    }
-    
-    public function test_evaluator_can_define_external_conditions()
-    {
-        $evaluator = new Evaluator();
-
-        $evaluator->defineCondition('custom', Condiment\Evaluables\Conditions\Definitions\Equals::class);
-
-        $this->assertArrayHasKey('custom', $evaluator->conditionDefinitions);
+        $this->evaluator = new Evaluator();
     }
 
-    public function test_external_conditions_can_be_called_with_magic_methods()
+    // public function testEvaluatorCanNotBeEvaluatedWithoutConditions()
+    // {
+    //     $this->expectException(\Condiment\Exceptions\EvaluatorMissingConditionsException::class);
+    //     $this->evaluator->evaluate();
+    // }
+
+    public static function conditionsProvider()
     {
-        $evaluator = new Evaluator();
+        return [
+            'Two conditions are evaluate and returns expected value' => [
+                [
+                    ['equals', [1,1], Evaluator::AND_CONNECTOR, false],
+                    ['contains', ["Hello", "ll"], Evaluator::AND_CONNECTOR, false],
+                ],
+                true
+            ],
+            'Conditions are grouped correctly' => [
+                [
+                    ['equals', [1,2], Evaluator::AND_CONNECTOR, false],
+                    ['contains', ["Hello", "b"], Evaluator::AND_CONNECTOR, false],
+                    ['match', ["condition", "tio(n|nal)"], Evaluator::OR_CONNECTOR, false],
+                ],
+                true
+            ],
+            'Groups are created manually' => [
+                [
+                    ['equals', [1,2], Evaluator::AND_CONNECTOR, false],
+                    'and' => [
+                        ['contains', ["Hello", "b"], Evaluator::AND_CONNECTOR, false],
+                        ['match', ["condition", "tio(n|nal)"], Evaluator::OR_CONNECTOR, false]
+                    ],
+                ],
+                false
+            ],
+            'Nested groups can be added manually' => [
+                [
+                    ['equals', [1,2], Evaluator::AND_CONNECTOR, false],
+                    'and' => [
+                        ['contains', ["Hello", "b"], Evaluator::AND_CONNECTOR, false],
+                        ['match', ["condition", "tio(n|nal)"], Evaluator::OR_CONNECTOR, false],
+                        'or' => [
+                            ['endsWith', ["PHP", "HP"], Evaluator::AND_CONNECTOR, false],
+                            ['startsWith', ["PHP", "PH"], Evaluator::OR_CONNECTOR, false],
+                        ]
+                    ],
+                ],
+                false
+            ],
+            'N groups can be added nested' => [
+                [
+                    ['equals', [1,2], Evaluator::AND_CONNECTOR, false],
+                    'and' => [
+                        ['contains', ["Hello", "b"], Evaluator::AND_CONNECTOR, false],
+                        ['match', ["condition", "tio(n|nal)"], Evaluator::OR_CONNECTOR, false],
+                        'or' => [
+                            ['endsWith', ["PHP", "HP"], Evaluator::AND_CONNECTOR, false],
+                            ['startsWith', ["PHP", "PH"], Evaluator::OR_CONNECTOR, false],
+                        ]
+                    ],
+                ],
+                false
+            ]
 
-        $evaluator->defineCondition('custom', Condiment\Evaluables\Conditions\Definitions\Equals::class);
+        ];
+    }
 
-        $this->assertArrayHasKey('custom', $evaluator->conditionDefinitions);
+    /**
+     * @dataProvider conditionsProvider
+     */
+    public function testEvaluatorReturnsTrue(array $conditions, bool $expected)
+    {
+        $this->evaluator->addConditions($conditions);
+        $this->assertSame($expected, $this->evaluator->evaluate());
+    }
+
+    public function testEvaluatorThrowsExceptionForNoExistingCondition ()
+    {
+        $this->expectException(EvaluatorInvalidConditionException::class);
+
+        $this->evaluator->addCondition('equality', [9,9]);
     }
 }
