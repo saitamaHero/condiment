74e3fa4d99387c01b7297d111e84adc0e6429798
Dionicio Acevedo <tec.dionicioacevedo@gmail.com>
2025-03-18 22:06:00 -0400

    feat: get the needed tokens needed to addCondition from magic call method

--- src/Evaluables/Evaluators/FluentEvaluator.php
+++ src/Evaluables/Evaluators/FluentEvaluator.php
@@ -13,13 +13,19 @@ class FluentEvaluator extends Evaluator
     {
         //TODO maybe I should evaluate that any condition added to this method to
         // enforce the conditions, but the condition itself handles the error
-        $conditionName = $this->getConditionNameFromCall($name);
+        $tokens = $this->getTokens($name);
 
-        if (! method_exists($this, $conditionName) && key_exists($conditionName, $this->conditionDefinitions)) {
-            $connector = strpos($name, self::OR_CONNECTOR) !== 0 ? self::AND_CONNECTOR : self::OR_CONNECTOR;
-            $negate = stripos($name, self::NOT_CONNECTOR) !== false;
+        if (
+            ! method_exists($this, $tokens['condition'])
+            && $this->conditionExists($tokens['condition'])
+        ) {
+            $this->addCondition(
+                $tokens['condition'],
+                $arguments,
+                $tokens['connector'],
+                $tokens['negation']
+            );
 
-            $this->addCondition($conditionName, $arguments, $connector, $negate);
         } else {
             throw new \Exception("Illegal Method \"$name\" on " . static::class);
         }
@@ -27,8 +33,48 @@ class FluentEvaluator extends Evaluator
         return $this;
     }
 
-    protected function getConditionNameFromCall(string $methodName): string
+    /**
+     * Get the needed tokens from the method call made by magic method
+     *
+     * This returns any modifies applied to the condition for example
+     *
+     * orNotContains returns connector => 'or', negation => true, condition=> 'contains'
+     *
+     * AND connector is assumed by default to avoid things like andEquals, andNotEquals
+     * this conditions pass as equals(x, x) notEquals(x,x) for better verbosity
+     *
+     * @param string $methodName
+     *
+     * @return array
+     */
+    public function getTokens(string $methodName)
     {
-        return \lcfirst(\str_ireplace([self::OR_CONNECTOR, self::NOT_CONNECTOR], "", $methodName));
+        $conditionName = \str_ireplace(
+            [self::OR_CONNECTOR, self::NOT_CONNECTOR],
+            "",
+            $methodName
+        );
+
+        $chunks = array_map(
+            'strtolower',
+            preg_split("/(?=[A-Z])/", str_replace($conditionName, "", $methodName))
+        );
+
+        $chunkCount = count($chunks);
+
+        if ($chunkCount > 1) {
+            if (!in_array($chunks[0], [self::AND_CONNECTOR, self::OR_CONNECTOR])) {
+                //TODO work on a new Exception
+                throw new \Exception("Connectors AND, OR should goes first when used combined with NOT");
+            }
+        }
+
+        $tokens = [
+            'negation' => in_array(self::NOT_CONNECTOR, $chunks),
+            'connector' => in_array(self::OR_CONNECTOR, $chunks) ? self::OR_CONNECTOR : self::AND_CONNECTOR,
+            'condition' =>  \lcfirst($conditionName),
+        ];
+
+        return $tokens;
     }
 }
